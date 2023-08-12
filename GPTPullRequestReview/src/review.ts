0f15c17857079671ce88e985b0d0ad84bc5cb06c
larhrouch mustapha <larhrouch@gmail.com>
2023-08-12 12:03:35 +0100

    Add support for GPT 4 model (#31)
    Co-authored-by: larhrouch mustapha <mustapha.larhrouch@hahn-software.io>

--- GPTPullRequestReview/src/review.ts
+++ GPTPullRequestReview/src/review.ts
@@ -3,34 +3,39 @@ import { git } from './git';
 import { OpenAIApi } from 'openai';
 import { addCommentToPR } from './pr';
 import { Agent } from 'https';
+import * as tl from "azure-pipelines-task-lib/task";
 
 export async function reviewFile(targetBranch: string, fileName: string, httpsAgent: Agent, apiKey: string, openai: OpenAIApi | undefined, aoiEndpoint: string | undefined) {
   console.log(`Start reviewing ${fileName} ...`);
 
+  const defaultOpenAIModel = 'gpt-3.5-turbo';
   const patch = await git.diff([targetBranch, '--', fileName]);
 
-  const prompt = `
-            Act as a code reviewer of a Pull Request, providing feedback on the code changes below.
-            You are provided with the Pull Request changes in a patch format.
-            Each patch entry has the commit message in the Subject line followed by the code changes (diffs) in a unidiff format.
-            
-            As a code reviewer, your task is:
-            - Review only added, edited or deleted lines.
-            - Non changed code should not be reviewed
-            - If there's no bugs, write 'No feedback'.
-            - Use bullet points if you have multiple comments.
-            
-            Patch of the Pull Request to review:
-            ${patch}
-            `;
+  const instructions = `Act as a code reviewer of a Pull Request, providing feedback on possible bugs and clean code issues.
+        You are provided with the Pull Request changes in a patch format.
+        Each patch entry has the commit message in the Subject line followed by the code changes (diffs) in a unidiff format.
+
+        As a code reviewer, your task is:
+                - Review only added, edited or deleted lines.
+                - If there's no bugs and the changes are correct, write only 'No feedback.'
+                - If there's bug or uncorrect code changes, don't write 'No feedback.'`;
 
   try {
     let choices: any;
 
     if (openai) {
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
+      const response = await openai.createChatCompletion({
+        model: tl.getInput('model') || defaultOpenAIModel,
+        messages: [
+          {
+            role: "system",
+            content: instructions
+          },
+          {
+            role: "user",
+            content: patch
+          }
+        ],
         max_tokens: 500
       });
 
@@ -55,7 +60,7 @@ export async function reviewFile(targetBranch: string, fileName: string, httpsAg
     }
 
     if (choices && choices.length > 0) {
-      const review = aoiEndpoint ? choices[0].message?.content : choices[0].text as string
+      const review = choices[0].message?.content as string;
 
       if (review.trim() !== "No feedback.") {
         await addCommentToPR(fileName, review, httpsAgent);
